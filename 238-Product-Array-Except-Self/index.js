b8ebffb924cdc1c97775c3f9407da903a82a1405
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-10-20 22:27:02 +0300

    Add: solution: 238 Product of Array Except Self

---
+++ 238-Product-Array-Except-Self/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let result = Array(nums.length).fill(1);
+
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] *= prefix;
+        prefix *= nums[i];
+    }
+
+    let suffix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+};
+
+productExceptSelf([1, 2, 3, 4]);
