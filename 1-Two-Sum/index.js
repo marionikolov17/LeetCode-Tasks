185d2e8d40cb81d0715797ef60baea23dc2296bb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-10-23 13:50:38 +0300

    Add: solution: 1 Two Sum

---
+++ 1-Two-Sum/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hashMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+
+        if (hashMap.has(diff)) {
+            return [hashMap.get(diff), i];
+        } else {
+            hashMap.set(nums[i], i);
+        }
+    }
+};
