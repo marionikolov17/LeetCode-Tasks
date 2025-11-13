14bb5be56b671d73c38cbc4f995b2164ed3e5946
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-11-13 16:37:39 +0200

    Add: solution: 67 Add Binary

---
+++ 67-Add-Binary/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    const numA = BigInt('0b' + a);
+    const numB = BigInt('0b' + b);
+    const sum = numA + numB;
+    return sum.toString(2);
+};
