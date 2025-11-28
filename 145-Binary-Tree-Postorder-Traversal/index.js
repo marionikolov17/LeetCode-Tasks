3adc35c245de3fffc372a49e9693561ad6cb6dd2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-11-28 14:38:08 +0200

    Add: solution: 145 Binary Tree Postorder Traversal

---
+++ 145-Binary-Tree-Postorder-Traversal/index.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    const res = [];
+    postorder(root, res);
+    return res;
+};
+
+var postorder = function(root, res) {
+    if (!root) return;
+
+    postorder(root.left, res);
+    postorder(root.right, res);
+    res.push(root.val);
+}
+
+// Left -> Right -> Root
