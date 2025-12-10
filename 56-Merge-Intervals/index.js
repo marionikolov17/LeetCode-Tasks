99fbe041d4012c62a75a2a5084b647bcf118ab03
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-12-10 14:55:52 +0200

    Add: solution: 56 Merge Intervals

---
+++ 56-Merge-Intervals/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [];
+
+    for (const interval of intervals) {
+        if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
+            merged.push(interval);
+        } else {
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1]);
+        }
+    }
+
+    return merged;
+};
